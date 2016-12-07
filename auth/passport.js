4967bd6d0687a5db42fca5eb8d0686e3a60f7fd6
Qi Zhang <zhang.qi.js@gmail.com>
2016-12-07 04:28:34 +0000

    Add auth folder

---
+++ auth/passport.js
@@ -0,0 +1,64 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/darsuser');
+
+/**
+ * Specifies what strategy we'll use
+ */
+module.exports = function (passport) {
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
+    // username is netid
+    passport.use('local-signup', new LocalStrategy({
+            usernameField: 'netid',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+        function (req, netid, password, done) {
+            User.findOne({'netid': netid}, function (err, user) {
+                if (err) {
+                    return done(err);
+                } else if (user) {
+                    return done(null, false);
+                } else {
+                    var newUser = new User();
+
+                    newUser.netid = netid;
+                    newUser.name = req.body.name;
+                    newUser.password = newUser.generateHash(password);
+                    newUser.graduationDate = req.body.graduationDate;
+                    newUser.classTaken = [];
+                    newUser.classInProgress = [];
+                    newUser.classRegistered = [];
+
+                    console.log("begin to save user to DB");
+                    newUser.save(function (err) {
+                        console.log("Oops, some err in saving!");
+                        return done(null, newUser);
+                    });
+                }
+
+            });
+        }));
+
+    passport.use('local-login', new LocalStrategy({
+            usernameField: 'netid',
+            passwordField: 'password'
+        },
+        function (netid, password, done) {
+            User.findOne({'netid': netid}, function (err, user) {
+                if (err) {
+                    return done(err);
+                } else if (!user || !user.validPassword(password)) {
+                    return done(null, false);
+                }
+                return done(null, user);
+            });
+        }));
+};
